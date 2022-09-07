e78afb5b43c3da26b4d1a3039d2c4beff1c6d9b4
Vadim <44135514+vodemn@users.noreply.github.com>
2022-09-07 19:16:51 +0300

    init UI
    added canvas
    added action buttons

--- DetectNumbers/ViewController.swift
+++ DetectNumbers/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    let canvas = Canvas()
+        
+    let clearButton: UIButton = {
+        let button  = UIButton(type: .system)
+        button.backgroundColor = UIColor.systemBlue
+        
+        let image = UIImage(systemName: "trash")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        button.setImage(image, for: .normal)
+        button.layer.cornerRadius = 16
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.widthAnchor.constraint(equalToConstant: 48).isActive = true
+        return button
+    }()
+    
+    let analyzeButton: UIButton = {
+        let button  = UIButton(type: .roundedRect)
+        button.backgroundColor = UIColor.systemBlue
+        button.layer.cornerRadius = 16
+        button.setTitle("Analyze", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let fullLayout: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        return stack
+    }()
+    
+    let actionsLayout: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.isLayoutMarginsRelativeArrangement = true
+        stack.layoutMargins = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        stack.distribution = .fillProportionally
+        stack.spacing = 16
+        return stack
+    }()
+    
+    override func viewSafeAreaInsetsDidChange() {
+        super.viewSafeAreaInsetsDidChange()
+        view.addSubview(fullLayout)
+        
+        fullLayout.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.frame.width, height: view.safeAreaLayoutGuide.layoutFrame.height)
+        actionsLayout.frame = CGRect(x: 0, y: view.frame.width, width: view.frame.width, height: 80)
+        
+        fullLayout.addSubview(canvas)
+        fullLayout.addSubview(actionsLayout)
+        actionsLayout.addArrangedSubview(analyzeButton)
+        actionsLayout.addArrangedSubview(clearButton)
+        
+        canvas.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width)
+        canvas.backgroundColor = .black
     }
-
-
 }
 
---
+++ DetectNumbers/Views/Canvas.swift
@@ -0,0 +1,58 @@
+//
+//  Canvas.swift
+//  DetectNumbers
+//
+//  Created by Vadim Turko on 8/16/22.
+//
+
+import Foundation
+import UIKit
+
+class Canvas: UIView {
+    var lines = [[CGPoint]]()
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        context.setStrokeColor(UIColor.white.cgColor)
+        context.setLineWidth(16)
+        context.setLineCap(.round)
+        
+        lines.forEach {
+            l in for (i, p) in l.enumerated() {
+                if (i == 0) {
+                    context.move(to: p)
+                } else {
+                    context.addLine(to: p)
+                }
+            }
+        }
+        
+        context.strokePath()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point = touches.first?.location(in: self) else { return }
+        guard var lastLine = lines.popLast() else { return }
+        lastLine.append(point)
+        lines.append(lastLine)
+        
+        setNeedsDisplay()
+    }
+    
+    func undo() {
+        lines.removeLast()
+        setNeedsDisplay()
+    }
+    
+    func clear() {
+        lines.removeAll()
+        setNeedsDisplay()
+    }
+}
